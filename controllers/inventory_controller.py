1063420b1bc399cf30b4758400ca99cde9f7e748
Stephen <stephendcorcoran@gmail.com>
2021-07-12 14:26:51 +0100

    created manufacturer class

--- controllers/inventory_controller.py
+++ controllers/inventory_controller.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, Blueprint
 
-from models.manufacturers import manufacturers
-from models.products import products
+from models.manufacturers import manufacturer
+from models.products import product
 
 import repositories.manufacturers_repository as manufacturers_repository
 import repositories.products_repositories as products_repositories
