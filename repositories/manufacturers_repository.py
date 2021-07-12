da07411e2b6f9ab3c755d9194348128cec725d2a
Stephen <stephendcorcoran@gmail.com>
2021-07-12 16:03:53 +0100

    Added save funtion to manufacturer_repository

--- repositories/manufacturers_repository.py
+++ repositories/manufacturers_repository.py
@@ -1,5 +1,12 @@
 from db.run_sql import run_sql
 
-from models.manufacturers import Manufacturers
-from models.products import Products
+from models.manufacturers import Manufacturer
+from models.products import Product
 
+def save(manufacturer):
+    sql = "INSERT INTO manufacturers (name) VALUES (%s) RETURNING"
+    values = [manufacturer.name]
+    results = run_sql(sql, values)
+    id = results[0] ['id']
+    manufacturer.id = id
+    return manufacturer
