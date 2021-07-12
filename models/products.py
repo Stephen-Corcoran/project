7bb2035f0bc1de7e6397bfed7f76099b2fe3f46d
Stephen <stephendcorcoran@gmail.com>
2021-07-12 14:38:10 +0100

    created product class

---
+++ models/products.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self, name, description, quantity, buy_price, sell_price, id=None):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+        self.buy_price = buy_price
+        self.sell_price = sell_price
+        self.id = id
+
+        
+    
