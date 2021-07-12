1ccc996cd1bfb00f84c80eaf0aff23fcf1ef3762
Stephen <stephendcorcoran@gmail.com>
2021-07-12 14:15:22 +0100

    Created tables in sql

--- db/inventory_manger.sql
+++ db/inventory_manger.sql
@@ -1,2 +1,16 @@
 DROP TABLE IF EXISTS manufacturers;
 DROP TABLE IF EXISTS products;
+
+CREATE TABLE manufacturers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255)
+)
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255),
+    description VARCHAR (255),
+    quantity INT,
+    buy_price INT,
+    sell_price INT,
+)
