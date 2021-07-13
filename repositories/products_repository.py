cf9f63741ab25b7187bd0ee83bc69c193b050adb
Stephen <stephendcorcoran@gmail.com>
2021-07-13 11:19:33 +0100

    Added update to products_repository

--- repositories/products_repository.py
+++ repositories/products_repository.py
@@ -21,3 +21,9 @@ def delete(id):
     sql = "DELETE FROM products WHERE id=%s"
     values =[id]
     run_sql(sql, values)
+
+def update(product):
+    sql = "UPDATE products SET (name, description, quantity, buy_price, sell_price) = (%s, %s, %s, %s, %s) WHERE id = %s"
+    values = [product.name, product.description, product.quantity, product.buy_price, product.sell_price, ]
+    print(values)
+    run_sql(sql, values)
