dc17452a43f65471349e3fb3edb73888320c5a3f
Stephen <stephendcorcoran@gmail.com>
2021-07-13 11:53:43 +0100

    Added select to manufacturers_repository

--- repositories/manufacturers_repository.py
+++ repositories/manufacturers_repository.py
@@ -25,6 +25,16 @@ def select_all():
         manufacturers.append(manufacturer)
     return manufacturers
 
+def select(id):
+    manufacturer = None
+    sql = "SELECT * FROM manufacturers WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        manufacturer = Manufacturer(result['name'], result['id'])
+    return manufacturer
+
 def delete_all():
     sql = "DELETE FROM manufacturers"
     run_sql(sql)
