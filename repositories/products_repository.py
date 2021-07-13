e2e7ad5e15a7f5f0ba086c7ded62fb70713d7175
Stephen <stephendcorcoran@gmail.com>
2021-07-13 11:10:09 +0100

    Added delete one to products_repository

--- repositories/products_repository.py
+++ repositories/products_repository.py
@@ -16,3 +16,8 @@ def save(product):
 def delete_all():
     sql = "DELETE FROM products"
     run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM products WHERE id=%s"
+    values =[id]
+    run_sql(sql, values)
