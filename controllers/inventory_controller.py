2aa82a81ca1c331cad6590595832257e8df7cab5
Stephen <stephendcorcoran@gmail.com>
2021-07-13 15:07:32 +0100

    Fixed bugs and created manufacturers/index.html

--- controllers/inventory_controller.py
+++ controllers/inventory_controller.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, Blueprint
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
 
 from models.manufacturers import Manufacturer
 from models.products import Product
@@ -6,3 +7,10 @@ from models.products import Product
 import repositories.manufacturers_repository as manufacturers_repository
 import repositories.products_repository as products_repositories
 
+manufacturers_blueprint = Blueprint("manufacturers", __name__)
+products_blueprint = Blueprint("products", __name__)
+
+@manufacturers_blueprint.route("/manufacturers")
+def manufacturers():
+    manufacturers = manufacturers_repository.select_all()
+    return render_template("manufacturers/index.html", all_manufacturers = manufacturers)
